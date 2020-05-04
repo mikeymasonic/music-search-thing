b61cbe34072868d471d893f4003d34308134be25
Michael Romay <amerror@gmail.com>
2020-05-04 14:40:22 -0700

    wrote input change, fetch artists, and on button click functions

--- src/containers/artist-search-container/FindArtistContainer.jsx
+++ src/containers/artist-search-container/FindArtistContainer.jsx
@@ -1,2 +1,41 @@
-// import React, { Component } from 'react';
-// import { getArtists } from '../../../services/getArtistsDetailsAPI';
+import React, { Component } from 'react';
+import { getArtists } from '../../../services/getArtistsDetailsAPI';
+
+
+export default class FindArtistsContainer extends Component {
+  static propTypes = {
+    history: this.propTypes.object,
+    location: this.propTypes.object
+  }
+
+  state = {
+    artistArray: [],
+    artist: '',
+    loading: false,
+    error: null,
+    page: 1,
+    totalPages: 1
+  }
+
+  onInputChange = ({ target }) => {
+    this.setState({ artist: target.value }, () => {
+      this.props.history.push(`/?query=${this.state.artist}`);
+    });
+  }
+
+  fetchArtists = () => {
+    return getArtists(this.state.artist, this.state.page)
+      .then(({ totalPages, singers }) => {
+        this.setState({
+          artistArray: singers,
+          loading: faulse,
+          totalPages: Math.ceil(totalPages / 25)
+        });
+      })
+      .catch(err => this.setState({
+        error: err,
+        loading: true
+      }));
+  }
+}
+
