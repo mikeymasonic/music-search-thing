97ccc41af1ca27d3fec522c93856ef0d22b47072
Michael Romay <amerror@gmail.com>
2020-05-05 12:25:18 -0700

    SongContainer finished

--- src/containers/song-container/SongContainer.jsx
+++ src/containers/song-container/SongContainer.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getSongs } from '../../services/getArtistsDetailsAPI';
+import Nav from '../nav/Nav';
+import Songs from '../../components/Songs/Songs';
 
 
 export default class SongContainer extends Component {
@@ -26,5 +28,34 @@ export default class SongContainer extends Component {
         error: err,
         loading: true
       }));
+  }
+
+  componentDidMount() {
+    this.fetchSongs();
+  }
+
+  render() {
+    const { songsArray, error, loading } = this.state;
+
+    if(error) return (
+      <div>
+        <Nav />
+        <h3>Sorry, songs are not available for this release...</h3>
+      </div>
+    );
+
+    if(loading) return (
+      <div>
+        <h3>loading...</h3>
+      </div>
+    );
+
+    return (
+      <>
+        <h2>Songs from <em>{this.props.match.params.releaseTitle}</em> by {this.props.match.params.artistName}</h2>
+        <Nav />
+        <Songs artistName={this.props.match.params.artistName} songsArray={songsArray} />
+      </>
+    );
   }
 }
