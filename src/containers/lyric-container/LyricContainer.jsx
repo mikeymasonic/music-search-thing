7a23164fd56c8eb35eb5cb2287d85b37adce49a2
Michael Romay <amerror@gmail.com>
2020-05-05 12:56:00 -0700

    finished up LyricContainer

--- src/containers/lyric-container/LyricContainer.jsx
+++ src/containers/lyric-container/LyricContainer.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getLyrics } from '../../services/getArtistsDetailsAPI';
+import Nav from '../nav/Nav';
+import Lyrics from '../../components/Lyrics/Lyrics';
 
 export default class LyricContainer extends Component {
   static propTypes = {
@@ -33,4 +35,32 @@ export default class LyricContainer extends Component {
     this.fetchLyrics();
   }
 
+  render() {
+    const {
+      lyrics,
+      error,
+      loading
+    } = this.state;
+
+    if(error) return (
+      <div>
+        <Nav />
+        <h3>Sorry, the lyrics for this song are not available...</h3>
+      </div>
+    );
+
+    if(loading) return (
+      <div>loading...</div>
+    );
+
+    return (
+      <div>
+        <Lyrics 
+          title={this.props.match.params.songTitle}
+          lyrics={lyrics}
+          artistName={this.props.match.params.artistName}
+        />
+      </div>
+    );
+  }
 }
