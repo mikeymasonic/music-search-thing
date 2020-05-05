446b62a26d76e43e56f49c905e8548b26cad3ac3
Michael Romay <amerror@gmail.com>
2020-05-05 12:43:25 -0700

    added Lyrics model

---
+++ src/components/Lyrics/Lyrics.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Nav from '../containers/nav/Nav';
+
+const Lyrics = ({ title, lyrics, artistName }) => {
+  return (
+    <section>
+      <h3>Lyrics for <em>{title}</em> by {artistName}</h3>
+      <Nav />
+      <pre>{lyrics}</pre>
+    </section>
+  );
+};
+
+Lyrics.propTypes = {
+  title: PropTypes.string.isRequired,
+  lyrics: PropTypes.string.isRequired,
+  artistName: PropTypes.string.isRequired
+};
+
+export default Lyrics;
